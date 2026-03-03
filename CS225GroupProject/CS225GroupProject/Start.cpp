58fc453b7b303ba9a108b3e2281dfdaf43429a84
d-pratt22 <156544542+d-pratt22@users.noreply.github.com>
2026-03-03 09:07:23 -0700

    Start of project

---
+++ CS225GroupProject/CS225GroupProject/Start.cpp
@@ -0,0 +1,36 @@
+//Start of Project!!
+
+/*
+
+What is the game
+-Control army to conquer map
+-Build up army with different units to battle other armies (type object pattern?)
+-AI agent for the enemy armies for them to move around (decision trees?)
+-Similar to Total War games
+
+*/
+
+/*
+
+To Do List
+-Set up the army builder
+--Possibly type object to make creation easier
+
+-Need the following parameters for the army/units
+--Size of army
+--Type of units in army
+--Amount of each unit in army
+--Strength of the army
+--Strength of individual units
+
+-Create the army class
+
+-Create class and maybe use case diagram before beginning coding
+
+-Make a simmple AI agent to control enemy armies
+--Use something like a decision tree or simple neural network
+
+-Start visuals
+--Install some packages for graphics
+
+*/
